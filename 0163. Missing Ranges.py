7a945c3a8e4ed2fb559d94501aed2ce3c6601ac0
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-05-29 08:17:39 +0600

    Array

--- 0163. Missing Ranges.py
+++ 0163. Missing Ranges.py
@@ -3,19 +3,14 @@ from typing import *
 class Solution:
     def findMissingRanges(self, nums: List[int], lower: int, upper: int) -> List[str]:
         res = []
-        curr = lower
-        nums.append(upper+1)
+        nums = [lower-1] + nums + [upper+1]
         
-        for i in range(len(nums)):
-            delta = nums[i] - curr
+        for i in range(len(nums)-1):
+            delta = nums[i+1] - nums[i]
             
-            if delta <= 0:
-                curr = nums[i] + 1
-            elif delta == 1:
-                res.append(str(curr))
-                curr = nums[i] + 1
-            else:
-                res.append(str(curr) + '->' + str(nums[i]-1))
-                curr = nums[i]+1
+            if delta == 2:
+                res.append(str(nums[i]+1))
+            elif delta > 2:
+                res.append(str(nums[i]+1) + '->' + str(nums[i+1]-1))
         
         return res
