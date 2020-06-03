975cd4ffa1ec1ea98a604089097f894723d17dfa
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-06-03 08:45:19 +0600

    Divide and Conquer

--- 0023. Merge k Sorted Lists.py
+++ 0023. Merge k Sorted Lists.py
@@ -7,18 +7,34 @@ from typing import *
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        import heapq
-        heap = []
+        def merge(l1, l2):
+            res = head = ListNode(-1)
+            
+            while l1 and l2:
+                if l1.val < l2.val:
+                    res.next = l1
+                    res = res.next
+                    l1 = l1.next
+                else:
+                    res.next = l2
+                    res = res.next
+                    l2 = l2.next
+            
+            if l1:
+                res.next = l1
+            
+            if l2:
+                res.next = l2
+            
+            return head.next
         
-        for l in lists:
-            while l:
-                heapq.heappush(heap, l.val)
-                l = l.next
+        n_lists = len(lists)
+        delta = 1
         
-        curr = head = ListNode(-1)
+        while delta < n_lists:
+            for i in range(0, n_lists - delta, delta * 2):
+                lists[i] = merge(lists[i], lists[i+delta])
+            
+            delta *= 2
         
-        while heap:
-            curr.next = ListNode(heapq.heappop(heap))
-            curr = curr.next
-        
-        return head.next
+        return lists[0] if n_lists else None
