5a001498f4232506b16d0ae083eb6cbdf513c698
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-06-07 16:54:08 +0600

    Heap shorter

--- 0253. Meeting Rooms II.py
+++ 0253. Meeting Rooms II.py
@@ -2,18 +2,15 @@ from typing import *
 
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals:
-            return 0
-        
-        rooms = 0
-        starts = sorted([x[0] for x in intervals])
-        ends = sorted([x[1] for x in intervals])
-        e_ptr = 0
-        
-        for s_ptr in range(len(starts)):
-            if starts[s_ptr] >= ends[e_ptr]:
-                e_ptr += 1
+        import heapq
+
+        heap = []
+        intervals.sort()
+
+        for start, end in intervals:
+            if heap and heap[0] <= start:
+                heapq.heappushpop(heap, end)
             else:
-                rooms += 1
+                heapq.heappush(heap, end)
         
-        return rooms
+        return len(heap)
