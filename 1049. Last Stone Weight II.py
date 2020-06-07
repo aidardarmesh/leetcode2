7743f7c37681098604ffc293ead611ccde0ab9eb
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-06-07 23:18:24 +0600

    01 Knapsack DP

--- 1049. Last Stone Weight II.py
+++ 1049. Last Stone Weight II.py
@@ -2,18 +2,22 @@ from typing import *
 
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        if not stones:
-            return 0
+        n, S = len(stones), sum(stones)
+        S2 = S // 2
+        dp = [[False for _ in range(S2+1)] for __ in range(n+1)]
+        dp[0][0] = True
+        max_S = 0
         
-        smashed_stones = {stones[0]}
+        for i in range(1, n+1):
+            for s in range(1, S2+1):
+                if dp[i-1][s] or (s >= stones[i-1] and dp[i-1][s-stones[i-1]]):
+                    dp[i][s] = True
+                    max_S = max(max_S, s)
         
-        for i in range(1, len(stones)):
-            temp = set()
-            
-            for smashed in smashed_stones:
-                temp.add(abs(smashed - stones[i]))
-                temp.add(smashed + stones[i])
-            
-            smashed_stones = temp
-        
-        return min(smashed_stones)
+        return abs(S - 2*max_S)
+
+s = Solution()
+
+assert s.lastStoneWeightII([2,7,4,1,8,1]) == 1
+assert s.lastStoneWeightII([8,2,4,4,8]) == 2
+assert s.lastStoneWeightII([31,26,33,21,40]) == 5
