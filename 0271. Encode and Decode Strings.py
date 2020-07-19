89495d55c18c61f98d90e6ea423521e25a59c032
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-07-19 18:03:51 +0600

    String Length Tokenizer

--- 0271. Encode and Decode Strings.py
+++ 0271. Encode and Decode Strings.py
@@ -1,18 +1,35 @@
 from typing import *
 
 class Codec:
+    LEN_SEP = '_'
+    META_DATA_SEP = '|'
+  
     def encode(self, strs: [str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        if not strs:
-            return chr(257)
-        
-        return chr(258).join(strs)
+        lens = self.LEN_SEP.join([str(len(word)) for word in strs])
+        data = ''.join(strs)
+
+        return lens + self.META_DATA_SEP + data
+
 
     def decode(self, s: str) -> [str]:
         """Decodes a single string to a list of strings.
         """
-        if s == chr(257):
+        meta_data_sep_idx = s.find(self.META_DATA_SEP)
+	
+        if not meta_data_sep_idx:
             return []
-        
-        return s.split(chr(258))
+
+        lens, data = s[:meta_data_sep_idx], s[meta_data_sep_idx+1:]
+        lens = lens.split(self.LEN_SEP)
+
+        ans = []
+        start_idx = 0
+
+        for word_len in lens:
+            end_idx = start_idx + int(word_len)
+            ans.append(data[start_idx:end_idx])
+            start_idx = end_idx
+
+        return ans
