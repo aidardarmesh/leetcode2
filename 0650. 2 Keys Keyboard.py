3b9e77e3ae72780ca0b5a6a8fff6f0d81cf29380
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-07-23 09:24:17 +0600

    Prime Factorization

--- 0650. 2 Keys Keyboard.py
+++ 0650. 2 Keys Keyboard.py
@@ -2,19 +2,15 @@ from typing import *
 
 class Solution:
     def minSteps(self, n: int) -> int:
-        if n == 1:
-            return 0
+        ans = 0
+        d = 2
         
-        dp = [0]*(n+1)
-        dp[2] = 2
-        
-        for i in range(2, n+1):
-            ans = i
+        while n != 1:
+            while n % d == 0:
+                ans += d
+                n //= d
             
-            for j in range(i-1, 1, -1):
-                if i % j == 0:
-                    ans = min(ans, dp[j] + i // j)
-            
-            dp[i] = ans
+            d += 1
         
-        return dp[n]
+        return ans
+
