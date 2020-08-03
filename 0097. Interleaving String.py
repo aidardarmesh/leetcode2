dd8f24bc1a8309ed663d4e81a32ac1fa992321be
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-08-03 14:54:59 +0600

    Recursion,Memoization

--- 0097. Interleaving String.py
+++ 0097. Interleaving String.py
@@ -2,22 +2,28 @@ from typing import *
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        r, c, l= len(s1), len(s2), len(s3)
-        
-        if r+c != l:
+        if len(s1) + len(s2) != len(s3):
             return False
-
-        dp = [[True for _ in range(c+1)] for _ in range(r+1)]
-
-        for i in range(1, r+1):
-            dp[i][0] = dp[i-1][0] and s1[i-1] == s3[i-1]
-
-        for j in range(1, c+1):
-            dp[0][j] = dp[0][j-1] and s2[j-1] == s3[j-1]
-
-        for i in range(1, r+1):
-            for j in range(1, c+1):
-                dp[i][j] = (dp[i-1][j] and s1[i-1] == s3[i-1+j]) or \
-                   (dp[i][j-1] and s2[j-1] == s3[i-1+j])
         
-        return dp[-1][-1]
+        memo = [[-1 for _ in range(len(s2)+1)] for __ in range(len(s1)+1)]
+        
+        def concat(s1, s2, s3, i, j, k, memo):
+            if memo[i][j] >= 0:
+                return True if memo[i][j] == 1 else False
+            
+            if i == len(s1) and j == len(s2) and k == len(s3):
+                return True
+            
+            ans = False
+            
+            if i < len(s1) and s1[i] == s3[k]:
+                ans |= concat(s1, s2, s3, i+1, j, k+1, memo)
+            
+            if j < len(s2) and s2[j] == s3[k]:
+                ans |= concat(s1, s2, s3, i, j+1, k+1, memo)
+            
+            memo[i][j] = 1 if ans else 0
+            
+            return ans
+        
+        return concat(s1, s2, s3, 0, 0, 0, memo)
