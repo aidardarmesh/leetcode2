fd9e8c94c1742bfb68afa67ab6cad637d936f2e8
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-09-25 22:13:19 +0600

    Better implementation

--- 0041. First Missing Positive.py
+++ 0041. First Missing Positive.py
@@ -3,24 +3,17 @@ from typing import *
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         # cyclic-sort
-        i = 0
-        while i < len(nums):
-            if nums[i] > 0 and nums[i] < len(nums) and nums[i] != i+1:
-                j = nums[i]-1
-                
-                # checking pair in correct place
-                if nums[j] == j+1:
-                    i += 1
-                    continue
-                    
-                nums[i], nums[j] = nums[j], nums[i]
-            else:
+        i, end = 0, len(nums)
+        while i != end:
+            val = nums[i]
+            
+            if val == i+1:
                 i += 1
+            elif val > end or val <= 0 or nums[val-1] == val:
+                end -= 1
+                nums[i], nums[end] = nums[end], nums[i]
+            else:
+                nums[i], nums[val-1] = nums[val-1], nums[i]
         
-        # search for number that is not in place
-        for i in range(len(nums)):
-            if nums[i] != i+1:
-                return i+1
-        
-        return len(nums)+1
+        return i+1
 
