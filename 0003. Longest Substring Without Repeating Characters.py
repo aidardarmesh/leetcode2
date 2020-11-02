6c051210bf069c41407e64b2ebbbcbd17a36b5c0
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-11-02 14:11:48 +0600

    Sliding Windows: Corner Case

--- 0003. Longest Substring Without Repeating Characters.py
+++ 0003. Longest Substring Without Repeating Characters.py
@@ -2,21 +2,15 @@ from typing import *
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        res = 0
+        left = -1
         char_idx = {}
-        max_len = 0
-        left_bound = -1
         
-        for i in range(len(s)):
-            if s[i] in char_idx:
-                left_bound = char_idx[s[i]]
-                new_char_idx = {}
-                
-                for char in char_idx:
-                    if char_idx[char] >= left_bound:
-                        new_char_idx[char] = char_idx[char]
-                
-                char_idx = new_char_idx
-            char_idx[s[i]] = i
-            max_len = max(max_len, i - left_bound)
+        for right in range(len(s)):
+            if s[right] in char_idx:
+                left = max(left, char_idx[s[right]])
+            
+            char_idx[s[right]] = right
+            res = max(res, right-left)
         
-        return max_len
+        return res
