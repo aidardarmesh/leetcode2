ee63fb5426bf3f81458825c916f72389242e0f20
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-11-10 12:11:57 +0600

    Heap

--- 0973. K Closest Points to Origin.py
+++ 0973. K Closest Points to Origin.py
@@ -2,9 +2,21 @@ from typing import *
 
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        import functools
+        import heapq
         
-        def comparator(p1, p2):
-            return (p1[0]**2 + p1[1]**2) - (p2[0]**2 + p2[1]**2)
+        heap = []
+        x_origin, y_origin = 0, 0
         
-        return sorted(points, key=functools.cmp_to_key(comparator))[:K]
+        for i in range(K):
+            x, y = points[i][0], points[i][1]
+            dist = (x_origin-x)**2 + (y_origin-y)**2
+            heapq.heappush(heap, (-dist, x, y))
+        
+        for i in range(K, len(points)):
+            x, y = points[i][0], points[i][1]
+            dist = (x_origin-x)**2 + (y_origin-y)**2
+            if dist < -heap[0][0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, (-dist, x, y))
+        
+        return [[x,y] for dist, x, y in heap]
