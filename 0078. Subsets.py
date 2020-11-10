da0a8db6e63fb05c4b45aa62aa26cf6c0647bd59
Aidar Darmesh <darmesh.aidar@gmail.com>
2020-11-10 14:57:38 +0600

    BFS

--- 0078. Subsets.py
+++ 0078. Subsets.py
@@ -2,14 +2,10 @@ from typing import *
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        res = [[]]
         
-        def generate(subset, idx, nums, res):
-            res.append(subset)
-            
-            for i in range(idx, len(nums)):
-                generate(subset[:] + [nums[i]], i+1, nums, res)
-        
-        generate([], 0, nums, res)
+        for num in nums:
+            for i in range(len(res)):
+                res.append(res[i] + [num])
         
         return res
